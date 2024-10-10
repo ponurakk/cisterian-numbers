55e0963c50cb5b0d3190e38e85412b426db37472
ponurakk <p0unrakg@int.pl>
2024-10-10 14:12:09 +0200

    fix: Fix returning shape

--- src/vertices.c
+++ src/vertices.c
@@ -1,81 +1,68 @@
 #include "vertices.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 Shape to10(Shape shape) {
-  float *new_vertices = malloc(shape.vertices.size * sizeof(float));
-  if (!new_vertices) {
-    perror("Failed to allocate memory for new vertices");
-  }
+  Vertices v = {.arr = malloc(shape.vertices.size * sizeof(float)),
+                .size = shape.vertices.size};
 
   for (size_t i = 0; i < shape.vertices.size; i++) {
-    new_vertices[i] = shape.vertices.arr[i];
+    v.arr[i] = shape.vertices.arr[i];
   }
 
   if (shape.vertices.size > 15)
-    new_vertices[15] = -new_vertices[15];
+    v.arr[15] = -v.arr[15];
   if (shape.vertices.size > 21)
-    new_vertices[21] = -new_vertices[21];
+    v.arr[21] = -v.arr[21];
 
-  Shape new_shape = {
-      .vertices = {.arr = new_vertices, .size = sizeof(new_vertices)},
-      .indices = shape.indices};
+  Shape new_shape = {.vertices = v, .indices = shape.indices};
 
   return new_shape;
 }
 
 Shape to100(Shape shape) {
-  float *new_vertices = malloc(shape.vertices.size * sizeof(float));
-  if (!new_vertices) {
-    perror("Failed to allocate memory for new vertices");
-  }
+  Vertices v = {.arr = malloc(shape.vertices.size * sizeof(float)),
+                .size = shape.vertices.size};
 
   for (size_t i = 0; i < shape.vertices.size; i++) {
-    new_vertices[i] = shape.vertices.arr[i];
+    v.arr[i] = shape.vertices.arr[i];
   }
 
   if (shape.vertices.size > 13)
-    new_vertices[13] = new_vertices[13] - 0.49f;
+    v.arr[13] = v.arr[13] - 0.49f;
   if (shape.vertices.size > 16)
-    new_vertices[16] = new_vertices[16] - 0.49f;
+    v.arr[16] = v.arr[16] - 0.49f;
   if (shape.vertices.size > 19)
-    new_vertices[19] = new_vertices[19] - 0.49f;
+    v.arr[19] = v.arr[19] - 0.49f;
   if (shape.vertices.size > 22)
-    new_vertices[22] = new_vertices[22] - 0.49f;
+    v.arr[22] = v.arr[22] - 0.49f;
 
-  Shape new_shape = {
-      .vertices = {.arr = new_vertices, .size = sizeof(new_vertices)},
-      .indices = shape.indices};
+  Shape new_shape = {.vertices = v, .indices = shape.indices};
 
   return new_shape;
 }
 
 Shape to1000(Shape shape) {
-  float *new_vertices = malloc(shape.vertices.size * sizeof(float));
-  if (!new_vertices) {
-    perror("Failed to allocate memory for new vertices");
-  }
+  Vertices v = {.arr = malloc(shape.vertices.size * sizeof(float)),
+                .size = shape.vertices.size};
 
   for (size_t i = 0; i < shape.vertices.size; i++) {
-    new_vertices[i] = shape.vertices.arr[i];
+    v.arr[i] = shape.vertices.arr[i];
   }
 
   if (shape.vertices.size > 13)
-    new_vertices[13] = new_vertices[13] - 0.49f;
+    v.arr[13] = v.arr[13] - 0.49f;
   if (shape.vertices.size > 16)
-    new_vertices[16] = new_vertices[16] - 0.49f;
+    v.arr[16] = v.arr[16] - 0.49f;
   if (shape.vertices.size > 19)
-    new_vertices[19] = new_vertices[19] - 0.49f;
+    v.arr[19] = v.arr[19] - 0.49f;
   if (shape.vertices.size > 22)
-    new_vertices[22] = new_vertices[22] - 0.49f;
+    v.arr[22] = v.arr[22] - 0.49f;
   if (shape.vertices.size > 15)
-    new_vertices[15] = -new_vertices[15];
+    v.arr[15] = -v.arr[15];
   if (shape.vertices.size > 21)
-    new_vertices[21] = -new_vertices[21];
+    v.arr[21] = -v.arr[21];
 
-  Shape new_shape = {
-      .vertices = {.arr = new_vertices, .size = sizeof(new_vertices)},
-      .indices = shape.indices};
+  Shape new_shape = {.vertices = v, .indices = shape.indices};
 
   return new_shape;
 }
